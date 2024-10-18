11330c0342c8f22687a077acb6c689499fbabc1c
kathumphrey <160188719+kathumphrey@users.noreply.github.com>
2024-10-18 22:09:57 +0000

    astro

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -149,21 +149,21 @@ declare module 'astro:content' {
   slug: "about";
   body: string;
   collection: "pages";
-  data: any
+  data: InferEntrySchema<"pages">
 } & { render(): Render[".mdoc"] };
 "contact.mdoc": {
 	id: "contact.mdoc";
   slug: "contact";
   body: string;
   collection: "pages";
-  data: any
+  data: InferEntrySchema<"pages">
 } & { render(): Render[".mdoc"] };
 "hidden-page.mdoc": {
 	id: "hidden-page.mdoc";
   slug: "hidden-page";
   body: string;
   collection: "pages";
-  data: any
+  data: InferEntrySchema<"pages">
 } & { render(): Render[".mdoc"] };
 };
 "portfolioGroups": {
@@ -179,5 +179,5 @@ declare module 'astro:content' {
 
 	type AnyEntryMap = ContentEntryMap & DataEntryMap;
 
-	export type ContentConfig = never;
+	export type ContentConfig = typeof import("../src/content/config.js");
 }
